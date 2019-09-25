2ff0c78a7f5d74f52a55a94ab0a78e76daa5fa46
Petrovich1973 <designtorg@ya.ru>
2019-09-26 01:35:08 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const App = () => {
-  return (
-    <div className="App">
-          App
-    </div>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Wizard from "./components/Wizard";
+
+import {actionTypes as types, app_name} from './constants';
+
+const App = (props) => {
+
+    console.log(props)
+    const handleChangeCurrent = () => {
+        props.dispatch({
+            type: types.APP_UPDATE,
+            payload: {
+                currentStep: 22222222
+            }
+        })
+    }
+
+    return (
+        <React.Fragment>
+
+            <Header/>
+
+            <section id={'content'}>
+
+                <Wizard>
+                    <p>{app_name} — {props.app.currentStep}</p>
+                    <button onClick={handleChangeCurrent}>action</button>
+                </Wizard>
+
+            </section>
+
+            <Footer/>
+
+        </React.Fragment>
     );
 }
 
-export default App;
+export default connect(store => {
+    return {
+        app: store.app,
+    };
+})(App);
---
+++ src/components/Wizard/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wizard = props => (
+    <div className={'wizard'}>
+        {props.children}
+    </div>
+)
+
+export default Wizard
